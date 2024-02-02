b12ddf1f8b1217705dd81067581ca37d14881202
LuisC-git <castellanosr10715@gamil.com>
2024-02-01 21:28:57 -0600

    borrado comentario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,4 @@
             No hay posts, sigue a alguien para mostrar los posts.
         </x-slot:textPostVacio>
     </x-lista-post>
-    {{-- @forelse ($posts as $post)
-        <h1>{{$post->titulo }}</h1>
-    @empty
-        <p>No hay Post</p>
-    @endforelse --}}
 @endsection
